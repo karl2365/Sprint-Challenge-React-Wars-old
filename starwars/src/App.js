84756a1e01f0d2ca2c98bd2b7ace06e452e30ee4
Karl Beksa <karlbeksa@gmail.com>
2019-05-31 13:01:38 -0400

    MVP Complete

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -33,7 +33,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="Header">React Wars</h1>
+        <h1 className="Header">React Wars Characters</h1>
         <div className="charContainer">
           {this.state.starwarsChars.map(char => (
             <DisplayChar char={char} />
--- starwars/src/components/DisplayChar.js
+++ starwars/src/components/DisplayChar.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import './StarWars.css';
-import DisplayFilm from './DisplayFilm';
 
 const DisplayChar = props => {
 
     return (
         <div className="character">
-           <p>Name: {props.char.name}</p> 
-            <p>Gender: {props.char.gender}</p>
-            <p>Hair Color: {props.char.hair_color}</p>
-            {/* <div class="films">
-                {props.char.films.map(film => (
-                    <DisplayFilm film={film} />
-                ))}
-            </div> */}
+            <p class="char-property">Name: {props.char.name}</p> 
+            <p class="char-property">Gender: {props.char.gender}</p>
+            <p class="char-property">Hair Color: {props.char.hair_color}</p>
+
         </div>
     )
 }
